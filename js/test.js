363b2d525b227c139797770706d961eb8707db37
dlrl <dlrl@MacBook-Air.local>
2016-04-03 00:47:57 +0530

    added test.html for sam

--- js/test.js
+++ js/test.js
@@ -3,13 +3,8 @@ jQuery(document).ready(function() {
     jQuery('.active').show();
     jQuery('.tab-container .nav a').on('click', function(e)  {
         var currentAttrValue = jQuery(this).attr('href');
- 
-        // Show/Hide Tabs
         jQuery('.tab-container ' + currentAttrValue).show().siblings().hide();
- 
-        // Change/remove current tab to active
         jQuery(this).parent('li').addClass('active').siblings().removeClass('active');
- 
         e.preventDefault();
     });
 });
